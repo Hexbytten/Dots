a3ec6c9c6a85368188349f1c1788bad39afb58fb
Hexbytten <soundbyte-53@proton.me>
2025-02-26 20:41:51 -0800

    added wayland support & fixed build.sh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -117,6 +117,12 @@
   junction
   ];
 
+  # Wayland and such
+  programs.hyprland.enable = true;
+  programs.waybar.enable = true;
+  programs.xwayland.enable = true;
+
+  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
