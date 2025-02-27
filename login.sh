e09a2e7113e308d01e0832f47c5c4075deb10ebf
Hexbytten <soundbyte-53@proton.me>
2025-02-27 01:23:07 -0800

    added automatic login because it was pissing me off

--- login.sh
+++ login.sh
@@ -1,6 +1,13 @@
+
+git config --global credential.credentialStore secretservice
+
 git-credential-manager --no-ui github login
 
 git-credential-manager configure
 
 git config --global user.email "soundbyte-53@proton.me"
 git config --global user.name "Hexbytten"
+
+git config --global --replace-all credential.helper manager
+
+echo "All done!"
