309ba75854844830b9cc9bfeff208e54135a256c
Hexbytten <soundbyte-53@proton.me>
2025-02-27 01:58:19 -0800

    Updated build.sh with colors

--- bao/build.sh
+++ bao/build.sh
@@ -1,8 +1,11 @@
 echo "Placing blocks and shit..."
 sleep 3;
 
+cyan='\033[0;36m'
+nc='\033[0m'
+
 sudo nixos-rebuild switch --flake .#nixos-mochi &&
 
-echo "Now just commit to main and push the changes!" &&
+echo -e "${cyan}Now just commit to main and push the changes!${nc}" &&
 
 sh ../commit.sh
